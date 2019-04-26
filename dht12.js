14eafe522c6499d14651a0b4dbdbef08c9488547
cf-programmers <45208584+cf-programmers@users.noreply.github.com>
2019-04-26 14:54:48 -0400

    Update dht12.js

--- dht12.js
+++ dht12.js
@@ -1,4 +1,25 @@
- var i2c = require('i2c');
+/*   Copyright (C) 2019 cf_programmers@yahoo.com
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/ 
+
+
+
+
+
+
+var i2c = require('i2c');
 
 var address = 0x5c;
 var wire = new i2c(address, {device: '/dev/i2c-2'}); // point to your i2c address
